ceec1835398b9de11e20516f6b45ff80b06a5a10
einre <einrebben880@gmail.com>
2024-11-25 02:17:07 +0800

    Commit

--- DelaCruz_2ndYear/src/bebi.java
+++
@@ -1,5 +0,0 @@
-public class bebi {
-    public static void main(String[] args) {
-        System.out.println("argjaoga");
-    }
-}
--- DelaCruz_2ndYear/src/ily.java
+++
@@ -1,5 +0,0 @@
-public class ily {
-    public static void main(String[] args) {
-        System.out.println("I love you");
-    }
-}
